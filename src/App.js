eaa76cecf4dee5bf9d44d6a47d5b4328a4ded7ec
Arjun Sarao <arjunsarao727@gmail.com>
2022-09-02 10:07:45 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import { getAnalytics } from "firebase/analytics";
 
 function App() {
   const firebaseConfig = {
-    apiKey: "XXX",
+    apiKey: "AIzaSyDUclMzSAW3WlMNtGBaNNfnShCA9TN5WDI",
     authDomain: "matt-tutoring-website.firebaseapp.com",
     projectId: "matt-tutoring-website",
     storageBucket: "matt-tutoring-website.appspot.com",
